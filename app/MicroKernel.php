6990ff5fdba7c29200a76dfe16aaac686b32a5f8
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2021-07-22 10:55:11 +0200

    Move app/config to config

--- app/MicroKernel.php
+++ app/MicroKernel.php
@@ -91,8 +91,8 @@ class MicroKernel extends Kernel
      */
     protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
     {
-        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
-        $loader->load(__DIR__ . '/config/services.yml');
+        $loader->load(__DIR__ . '/../config/config_' . $this->getEnvironment() . '.yml');
+        $loader->load(__DIR__ . '/../config/services.yml');
     }
 
     /*
