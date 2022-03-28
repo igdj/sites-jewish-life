b0c5314bcb59e6567035b5dfda686f13e14b2593
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2022-03-28 09:25:24 +0200

    Switch to box/spout

--- src/Command/ImportCommand.php
+++ src/Command/ImportCommand.php
@@ -16,6 +16,8 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 use Doctrine\ORM\EntityManagerInterface;
 
+use Box\Spout\Reader\Common\Creator\ReaderEntityFactory;
+
 class ImportCommand extends Command
 {
     protected $em;
@@ -61,150 +63,170 @@ class ImportCommand extends Command
             return 1;
         }
 
-        $file = new \SplFileObject($fname);
-        $reader = new \Ddeboer\DataImport\Reader\ExcelReader($file);
+        $reader = ReaderEntityFactory::createReaderFromFile($fname);
 
-        $reader->setHeaderRowNumber(0);
+        $reader->open($fname);
+
         $count = 0;
 
         $siteRepository = $this->em->getRepository('\App\Entity\Site');
 
-        foreach ($reader as $row) {
-            $unique_values = array_unique(array_values($row));
-            if (1 == count($unique_values) && null === $unique_values[0]) {
-                // all values null
-                continue;
-            }
+        $headers = [];
 
-            if (empty($row['ID'])) {
-                continue;
-            }
+        foreach ($reader->getSheetIterator() as $sheet) {
+            foreach ($sheet->getRowIterator() as $rowObj) {
+                $values = $rowObj->toArray();
 
-            $output->writeln('Insert/Update: ' . $row['ID']);
-
-            $site = $siteRepository->findOneById($row['ID']);
-            if (is_null($site)) {
-                $site = new \App\Entity\Site();
-                $site->setId($row['ID']);
-            }
-
-            foreach ($row as $key => $value) {
-                switch ($key) {
-                    case 'marker':
-                    case 'street':
-                    case 'latitude':
-                    case 'longitude':
-                    case 'author':
-                    case 'url':
-                    case 'topic':
-                        $site->{$key} = trim($value);
-                        break;
-
-                    case 'title_de':
-                    case 'description_de':
-                    case 'title_en':
-                    case 'description_en':
-                    case 'streetoverride_en':
-                        list($field, $lang) = explode('_', $key, 2);
-                        if ('streetoverride' == $field) {
-                            $field = 'streetOverride';
-                        }
-
-                        $currentValues = $site->{$field};
-                        if (is_null($currentValues)) {
-                            $currentValues = [];
-                        }
-
-                        $currentValues[$lang] = trim($value);
-                        $site->{$field} = $currentValues;
-                        break;
-
-                    case 'dasjuedischehamburg':
-                    case 'related':
-                    case 'keydocuments':
-                        $currentValues = $site->additional;
-                        if (!empty($value)) {
-                            if (is_null($currentValues)) {
-                                $currentValues = [];
-                            }
-
-                            if ($key == 'related') {
-                                $currentValues[$key] =  preg_split('/\s*,\s*/', $value);
-                            }
-                            else if ($key == 'keydocuments') {
-                                $currentValues[$key] = $this->buildKeydocuments(preg_split('/\s*;\s*/', $value));
-                            }
-                            else {
-                                $value = preg_replace('#https?://[www\.]*dasjuedischehamburg\.de/inhalt/#', '', $value);
-
-                                $currentValues[$key] = $this->buildJuedischeHamburg(preg_split('/\s*;\s*/', $value));
-                            }
-
-                        }
-                        else if (!is_null($currentValues)) {
-                            unset($currentValues[$key]);
-                        }
-
-                        $site->additional = $currentValues;
-                        break;
-
-                    case 'rights_1_de':
-                        $key = 'media';
-                        $currentValues = $site->additional;
-
-                        if (!empty($value)) {
-                            if (is_null($currentValues)) {
-                                $currentValues = [];
-                            }
-
-                            $mediaPath = '/web/media/';
-                            $media = [];
-
-                            $captions = preg_split('/\s*;\s*/', $value);
-                            $captions_en = preg_split('/\s*;\s*/', $row['rights_1_eng']);
-
-                            for ($i = 0; $i < count($captions); $i++) {
-                                $append = '';
-                                if (1 == $i) {
-                                    $append = 'b';
-                                }
-                                else if ($i > 1) {
-                                    die('TODO: Handle multiple images in ' . $row['ID']);
-                                }
-
-                                $fname = sprintf('ID_%s%s.jpg',
-                                                 $row['ID'], $append);
-                                if (!file_exists($this->rootDir . $mediaPath . $fname)) {
-                                    die($fname . ' does not exist');
-                                }
-
-                                $media[] = [
-                                    'de' => [
-                                        'url' => '/media/' . $fname,
-                                        'caption' => $captions[$i],
-                                    ],
-                                    'en' => [
-                                        'url' => '/media/' . $fname,
-                                        'caption' => $captions_en[$i],
-                                    ],
-                                ];
-                            }
-
-                            $currentValues[$key] = $media;
-                        }
-                        else if (!is_null($currentValues)) {
-                            unset($currentValues[$key]);
-                        }
-
-                        $site->additional = $currentValues;
-                        break;
-
-                    default:
-                        // $output->writeln('Skip : ' . $key);
+                $unique_values = array_unique(array_values($values));
+                if (1 == count($unique_values) && null === $unique_values[0]) {
+                    // all values null
+                    continue;
                 }
+
+                if (empty($headers)) {
+                    $headers = $values;
+                    continue;
+                }
+
+                // make assoc
+                $row = [];
+                foreach ($headers as $i => $key) {
+                    $row[$key] = array_key_exists($i, $values) ? trim($values[$i]) : null;
+                }
+
+
+                if (empty($row['ID'])) {
+                    continue;
+                }
+
+                $output->writeln('Insert/Update: ' . $row['ID']);
+
+                $site = $siteRepository->findOneById($row['ID']);
+                if (is_null($site)) {
+                    $site = new \App\Entity\Site();
+                    $site->setId($row['ID']);
+                }
+
+                foreach ($row as $key => $value) {
+                    switch ($key) {
+                        case 'marker':
+                        case 'street':
+                        case 'latitude':
+                        case 'longitude':
+                        case 'author':
+                        case 'url':
+                        case 'topic':
+                            $site->{$key} = trim($value);
+                            break;
+
+                        case 'title_de':
+                        case 'description_de':
+                        case 'title_en':
+                        case 'description_en':
+                        case 'streetoverride_en':
+                            list($field, $lang) = explode('_', $key, 2);
+                            if ('streetoverride' == $field) {
+                                $field = 'streetOverride';
+                            }
+
+                            $currentValues = $site->{$field};
+                            if (is_null($currentValues)) {
+                                $currentValues = [];
+                            }
+
+                            $currentValues[$lang] = trim($value);
+                            $site->{$field} = $currentValues;
+                            break;
+
+                        case 'dasjuedischehamburg':
+                        case 'related':
+                        case 'keydocuments':
+                            $currentValues = $site->additional;
+                            if (!empty($value)) {
+                                if (is_null($currentValues)) {
+                                    $currentValues = [];
+                                }
+
+                                if ($key == 'related') {
+                                    $currentValues[$key] =  preg_split('/\s*,\s*/', $value);
+                                }
+                                else if ($key == 'keydocuments') {
+                                    $currentValues[$key] = $this->buildKeydocuments(preg_split('/\s*;\s*/', $value));
+                                }
+                                else {
+                                    $value = preg_replace('#https?://[www\.]*dasjuedischehamburg\.de/inhalt/#', '', $value);
+
+                                    $currentValues[$key] = $this->buildJuedischeHamburg(preg_split('/\s*;\s*/', $value));
+                                }
+
+                            }
+                            else if (!is_null($currentValues)) {
+                                unset($currentValues[$key]);
+                            }
+
+                            $site->additional = $currentValues;
+                            break;
+
+                        case 'rights_1_de':
+                            $key = 'media';
+                            $currentValues = $site->additional;
+
+                            if (!empty($value)) {
+                                if (is_null($currentValues)) {
+                                    $currentValues = [];
+                                }
+
+                                $mediaPath = '/web/media/';
+                                $media = [];
+
+                                $captions = preg_split('/\s*;\s*/', $value);
+                                $captions_en = preg_split('/\s*;\s*/', $row['rights_1_eng']);
+
+                                for ($i = 0; $i < count($captions); $i++) {
+                                    $append = '';
+                                    if (1 == $i) {
+                                        $append = 'b';
+                                    }
+                                    else if ($i > 1) {
+                                        die('TODO: Handle multiple images in ' . $row['ID']);
+                                    }
+
+                                    $fname = sprintf('ID_%s%s.jpg',
+                                                     $row['ID'], $append);
+                                    if (!file_exists($this->rootDir . $mediaPath . $fname)) {
+                                        die($fname . ' does not exist');
+                                    }
+
+                                    $media[] = [
+                                        'de' => [
+                                            'url' => '/media/' . $fname,
+                                            'caption' => $captions[$i],
+                                        ],
+                                        'en' => [
+                                            'url' => '/media/' . $fname,
+                                            'caption' => $captions_en[$i],
+                                        ],
+                                    ];
+                                }
+
+                                $currentValues[$key] = $media;
+                            }
+                            else if (!is_null($currentValues)) {
+                                unset($currentValues[$key]);
+                            }
+
+                            $site->additional = $currentValues;
+                            break;
+
+                        default:
+                            // $output->writeln('Skip : ' . $key);
+                    }
+                }
+
+                $this->em->persist($site);
             }
 
-            $this->em->persist($site);
+            break; // only get first sheet
         }
 
         $this->em->flush();
